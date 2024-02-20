90ba2f4891d357d29b47a506e7d68c0c6c2caab6
[Divyansh Jain] <[jdivyansh51@gmail.com]>
2024-02-20 16:36:06 +0530

    final code

--- assignment2.cpp
+++ assignment2.cpp
@@ -16,19 +16,27 @@ int merge(int arr[], int l, int m, int r) {
     int i = 0, j = 0, k = l;
     while (i < n1 && j < n2) {
         if (L[i] <= R[j]) {
-            arr[k++] = L[i++];
+            arr[k] = L[i];
+            k++;
+            i++;
         } else {
-            arr[k++] = R[j++];
+            arr[k] = R[j];
+            k++;
+            j++;
         }
         comparisons++;
     }
 
     while (i < n1) {
-        arr[k++] = L[i++];
+        arr[k] = L[i];
+        k++;
+        i++;
     }
 
     while (j < n2) {
-        arr[k++] = R[j++];
+        arr[k] = R[j];
+        k++;
+        j++;
     }
 
     return comparisons;
@@ -41,7 +49,6 @@ long long mergeSort(int arr[], int l, int r) {
 
         comparisons += mergeSort(arr, l, m);
         comparisons += mergeSort(arr, m + 1, r);
-
         comparisons += merge(arr, l, m, r);
     }
     return comparisons;
@@ -51,17 +58,7 @@ int main() {
     int arr[] = {12, 11, 13, 5, 6, 7};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    cout << "Original array: ";
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-    cout << endl;
-
     long long comparisons = mergeSort(arr, 0, n - 1);
-
-    cout << "Sorted array: ";
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-    cout << endl;
 
     cout << "Number of comparisons: " << comparisons << endl;
 
@@ -69,23 +66,23 @@ int main() {
 }
 
 
-// --------
+// --------insertion sort
 
 
 #include <iostream>
 using namespace std;
 
 void insertionSort(int arr[], int n) {
-    for (int i = 1; i < n; i++) {
-        int key = arr[i];
-        int j = i - 1;
+    for (int j = 1; j < n; j++) {
+        int key = arr[j];
+        int i = j - 1;
 
         // Move elements of arr[0..i-1], that are greater than key, to one position ahead of their current position
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
+        while (i >= 0 && arr[i] > key) {
+            arr[i + 1] = arr[i];
+            i =i - 1;
         }
-        arr[j + 1] = key;
+        arr[i + 1] = key;
     }
 }
 
